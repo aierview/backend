f42bdc96e80087fa087919bfbd1fa233dc5ec582
gervasioartur <gervasioartur@outlook.com.br>
2025-08-04 04:35:32 -0300

    test: should save user, encode password and save auth details

--- src/test/java/com/aierview/backend/auth/application/usecase/impl/LocalSignupTests.java
+++ src/test/java/com/aierview/backend/auth/application/usecase/impl/LocalSignupTests.java
@@ -1,10 +1,14 @@
 package com.aierview.backend.auth.application.usecase.impl;
 
 import com.aierview.backend.auth.application.usecase.contract.ILocalSignup;
+import com.aierview.backend.auth.domain.entity.Auth;
 import com.aierview.backend.auth.domain.entity.UserRef;
+import com.aierview.backend.auth.domain.enums.AuthProvider;
 import com.aierview.backend.auth.domain.exceptions.EmailAlreadyInUseException;
 import com.aierview.backend.auth.domain.model.LocalSigninRequest;
+import com.aierview.backend.auth.domain.repository.IAuthRepository;
 import com.aierview.backend.auth.domain.repository.IUserRepository;
+import com.aierview.backend.auth.domain.security.IPasswordEncoder;
 import com.aierview.backend.shared.testdata.AuthTestFixture;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.DisplayName;
@@ -21,18 +25,22 @@ import static org.mockito.Mockito.*;
 public class LocalSignupTests {
     private ILocalSignup localSignup;
     private IUserRepository userRepository;
+    private IPasswordEncoder passwordEncoder;
+    private IAuthRepository authRepository;
 
     @BeforeEach
     void setUp() {
         this.userRepository = mock(IUserRepository.class);
-        this.localSignup = new LocalSignup(userRepository);
+        this.passwordEncoder = mock(IPasswordEncoder.class);
+        this.authRepository = mock(IAuthRepository.class);
+        this.localSignup = new LocalSignup(userRepository, passwordEncoder, authRepository);
     }
 
     @Test
     @DisplayName("Should throw EmailAlreadyInUseException if email is already in use")
     void shouldThrowEmailAlreadyInUseExceptionIfEmailIsAlreadyInUse() {
         LocalSigninRequest request = AuthTestFixture.anyLocalSigninRequest();
-        UserRef existingUser = AuthTestFixture.anyUserRef();
+        UserRef existingUser = AuthTestFixture.anySavedUserRef();
 
         when(this.userRepository.findByEmail(request.getEmail())).thenReturn(Optional.of(existingUser));
 
@@ -44,14 +52,25 @@ public class LocalSignupTests {
     }
 
     @Test
-    @DisplayName("Should save user ref, encode the password, save auth details")
+    @DisplayName("Should save user ref, encode the password and save auth details")
     void shouldSaveUserRefAndEncodePasswordAndSaveAuthDetails() {
         LocalSigninRequest request = AuthTestFixture.anyLocalSigninRequest();
-        UserRef existingUser = AuthTestFixture.anyUserRef();
+        UserRef toSaveUser = AuthTestFixture.anyUserRef();
+        UserRef savedUser = AuthTestFixture.anySavedUserRef();
+        Auth toSaveAuth = AuthTestFixture.anyAuth(savedUser);
+        Auth savedAuth = AuthTestFixture.anySavedAuth(savedUser);
 
         when(this.userRepository.findByEmail(request.getEmail())).thenReturn(Optional.empty());
+        when(this.userRepository.save(toSaveUser)).thenReturn(savedUser);
+        when(this.passwordEncoder.encode(request.getPassword())).thenReturn(toSaveAuth.getPassword());
+        when(this.authRepository.save(toSaveAuth)).thenReturn(savedAuth);
 
+        this.localSignup.execute(request);
 
+        verify(this.userRepository, times(1)).findByEmail(request.getEmail());
+        verify(this.userRepository, times(1)).save(toSaveUser);
+        verify(this.passwordEncoder, times(1)).encode(request.getPassword());
+        verify(this.authRepository, times(1)).save(toSaveAuth);
     }
 
 }
--- src/test/java/com/aierview/backend/shared/testdata/AuthTestFixture.java
+++ src/test/java/com/aierview/backend/shared/testdata/AuthTestFixture.java
@@ -1,23 +1,57 @@
 package com.aierview.backend.shared.testdata;
 
+import com.aierview.backend.auth.domain.entity.Auth;
 import com.aierview.backend.auth.domain.entity.UserRef;
+import com.aierview.backend.auth.domain.enums.AuthProvider;
 import com.aierview.backend.auth.domain.enums.UserRole;
 import com.aierview.backend.auth.domain.model.LocalSigninRequest;
 
+import java.util.UUID;
+
 public class AuthTestFixture {
+
     public static LocalSigninRequest anyLocalSigninRequest() {
         return LocalSigninRequest.builder()
                 .email("any_email")
                 .password("any_password")
+                .name("any_name")
+                .role(UserRole.FULLSTACK)
                 .build();
     }
 
     public static UserRef anyUserRef() {
         return UserRef.builder()
+                .name("any_name")
+                .email("any_email")
+                .role(UserRole.FULLSTACK)
+                .build();
+    }
+
+    public static UserRef anySavedUserRef() {
+        return UserRef.builder()
                 .id(1L)
                 .name("any_name")
                 .email("any_email")
                 .role(UserRole.FULLSTACK)
+                .build();
+    }
+
+    public static Auth anyAuth(UserRef savedUser) {
+        return  Auth
+                .builder()
+                .password(UUID.randomUUID().toString())
+                .provider(AuthProvider.LOCAL)
+                .user(savedUser)
+                .build();
+    }
+
+    public static Auth anySavedAuth(UserRef savedUser) {
+        return  Auth
+                .builder()
+                .id(1L)
+                .password(UUID.randomUUID().toString())
+                .provider(AuthProvider.LOCAL)
+                .user(savedUser)
                 .build();
     }
 }
