5471473bc3a4baeec25aaf338b76201ed54bb9d9
gervasioartur <gervasioartur@outlook.com.br>
2025-08-04 05:33:27 -0300

    test: ensure BCryptPasswordEncoderAdapter encodes password

---
+++ src/test/java/com/aierview/backend/auth/infra/security/BCryptPasswordEncoderAdapterTests.java
@@ -0,0 +1,39 @@
+package com.aierview.backend.auth.infra.security;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.BeforeEach;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+import org.mockito.Mockito;
+import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.security.crypto.password.PasswordEncoder;
+
+import java.util.UUID;
+
+import static org.mockito.Mockito.*;
+
+@SpringBootTest
+public class BCryptPasswordEncoderAdapterTests {
+    private BCryptPasswordEncoderAdapter adapter;
+    private PasswordEncoder encoder;
+
+
+    @BeforeEach
+    void setUp() {
+        this.encoder =  mock(PasswordEncoder.class);
+        this.adapter = new BCryptPasswordEncoderAdapter(encoder);
+    }
+
+    @Test
+    @DisplayName("Should encode password")
+    void shouldEncodePassword() {
+        String password = "anyPassword";
+        String encodedPassword = UUID.randomUUID().toString();
+        when(encoder.encode(password)).thenReturn(encodedPassword);
+
+        String encoded = adapter.encode(password);
+
+        Assertions.assertEquals(encodedPassword, encoded);
+        verify(encoder, Mockito.times(1)).encode(password);
+    }
+}
