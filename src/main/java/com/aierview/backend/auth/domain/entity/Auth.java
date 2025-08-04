02c55dba9b99ce23d4d35ce855a50b087f8ff620
gervasioartur <gervasioartur@outlook.com.br>
2025-08-04 03:08:13 -0300

    feat: adding auth entity and provider

---
+++ src/main/java/com/aierview/backend/auth/domain/entity/Auth.java
@@ -0,0 +1,18 @@
+package com.aierview.backend.auth.domain.entity;
+
+import com.aierview.backend.auth.domain.enums.AuthProvider;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+
+@Data
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class Auth {
+    private Long id;
+    private String password;
+    private AuthProvider provider;
+    private UserRef user;
+}
