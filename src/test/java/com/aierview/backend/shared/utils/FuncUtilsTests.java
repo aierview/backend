a37ea93069fe07f48a2d8d70fcd054c22ba9c270
gervasioartur <gervasioartur@outlook.com.br>
2025-08-04 23:28:05 -0300

    test: ensure FuncUtilsTests return capitalized text and lowercased text

---
+++ src/test/java/com/aierview/backend/shared/utils/FuncUtilsTests.java
@@ -0,0 +1,24 @@
+package com.aierview.backend.shared.utils;
+
+import org.junit.jupiter.api.Assertions;
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class FuncUtilsTests {
+
+    @Test
+    @DisplayName("Should return capitalized string")
+    void capitalize() {
+        String name = " john snow smith ";
+        String capitalized = FuncUtils.formatToCapitalize(name );
+        Assertions.assertEquals("John Snow Smith", capitalized);
+    }
+
+    @Test
+    @DisplayName("Should return lowercase string")
+    void lowerCase() {
+        String name = " john snow smith ";
+        String lowercase = FuncUtils.formatLowercase(name );
+        Assertions.assertEquals("john snow smith", lowercase);
+    }
+}
