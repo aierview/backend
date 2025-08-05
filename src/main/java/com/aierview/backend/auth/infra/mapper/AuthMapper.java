6477744eaa19a520bdf3decce1e048cb8ed5b50b
gervasioartur <gervasioartur@outlook.com.br>
2025-08-04 22:07:57 -0300

    chore: adding test coverage

--- src/main/java/com/aierview/backend/auth/infra/mapper/AuthMapper.java
+++ src/main/java/com/aierview/backend/auth/infra/mapper/AuthMapper.java
@@ -3,12 +3,9 @@ package com.aierview.backend.auth.infra.mapper;
 import com.aierview.backend.auth.domain.entity.Auth;
 import com.aierview.backend.auth.infra.persisntence.jpa.entity.AuthJpaEntity;
 import org.mapstruct.Mapper;
-import org.mapstruct.factory.Mappers;
 
 @Mapper(componentModel = "spring")
 public interface AuthMapper {
-    AuthMapper INSTANCE = Mappers.getMapper(AuthMapper.class);
-
     AuthJpaEntity authToAuthJpaEntity(Auth auth);
 
     Auth authJpaEntityToAuth(AuthJpaEntity auth);
