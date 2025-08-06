5e2e06e29f60b11af9038b1383f3f0531e5f06ab
gervasioartur <gervasioartur@outlook.com.br>
2025-08-06 01:31:33 -0300

    feat: ensure GenerateCookieResponse returns cookie response for prod env

---
+++ src/main/java/com/aierview/backend/auth/usecase/impl/GenerateCookieResponse.java
@@ -0,0 +1,20 @@
+package com.aierview.backend.auth.usecase.impl;
+
+import com.aierview.backend.auth.domain.model.CookieResponse;
+import com.aierview.backend.auth.usecase.contract.IGenerateCookieResponse;
+import com.aierview.backend.shared.enums.Environment;
+
+public class GenerateCookieResponse implements IGenerateCookieResponse {
+    private final Environment environment;
+
+    public GenerateCookieResponse(Environment environment) {
+        this.environment = environment;
+    }
+
+    @Override
+    public CookieResponse generate(String name, String value) {
+        String sameSite = this.environment.equals(Environment.PROD) ? "NONE" : "LAX";
+        boolean secure = this.environment.equals(Environment.PROD);
+        return new CookieResponse(name, value, true, secure, sameSite, "/");
+    }
+}
---
+++ src/main/java/com/aierview/backend/shared/enums/Environment.java
@@ -0,0 +1,7 @@
+package com.aierview.backend.shared.enums;
+
+public enum Environment {
+    PROD,
+    HOMOLOG,
+    DEV
+}
