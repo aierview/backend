2858bcd1b5f5cfecb7e95c2278307ed4f0e9e054
gervasioartur <gervasioartur@outlook.com.br>
2025-08-06 02:12:06 -0300

    feat: ensure BCryptPasswordEncoderAdapter return false if password does not match

--- src/main/java/com/aierview/backend/auth/infra/security/BCryptPasswordEncoderAdapter.java
+++ src/main/java/com/aierview/backend/auth/infra/security/BCryptPasswordEncoderAdapter.java
@@ -1,5 +1,6 @@
 package com.aierview.backend.auth.infra.security;
 
+import com.aierview.backend.auth.domain.security.IPasswordComparer;
 import com.aierview.backend.auth.domain.security.IPasswordEncoder;
 import lombok.RequiredArgsConstructor;
 import org.springframework.security.crypto.password.PasswordEncoder;
@@ -7,11 +8,17 @@ import org.springframework.stereotype.Component;
 
 @Component
 @RequiredArgsConstructor
-public class BCryptPasswordEncoderAdapter implements IPasswordEncoder {
+public class BCryptPasswordEncoderAdapter implements IPasswordEncoder, IPasswordComparer {
     private final PasswordEncoder encoder;
 
     @Override
     public String encode(String password) {
         return this.encoder.encode(password);
+    }
+
+    @Override
+    public boolean matches(String password, String hashedPassword) {
+        this.encoder.matches(password, hashedPassword);
+        return false;
     }
 }
