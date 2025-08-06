39924aa1460dadfd9b27156aa170ec4d1b0a9d3f
gervasioartur <gervasioartur@outlook.com.br>
2025-08-06 00:03:08 -0300

    test: ensure LocalSignin throw InvalidCredentialException when password does not match

--- src/test/java/com/aierview/backend/auth/usecase/impl/LocalSigninTests.java
+++ src/test/java/com/aierview/backend/auth/usecase/impl/LocalSigninTests.java
@@ -1,8 +1,12 @@
 package com.aierview.backend.auth.usecase.impl;
 
+import com.aierview.backend.auth.domain.entity.Auth;
+import com.aierview.backend.auth.domain.entity.UserRef;
 import com.aierview.backend.auth.domain.exceptions.InvalidCredentialException;
 import com.aierview.backend.auth.domain.model.LocalSigninRequest;
+import com.aierview.backend.auth.domain.repository.IAuthRepository;
 import com.aierview.backend.auth.domain.repository.IUserRepository;
+import com.aierview.backend.auth.domain.security.IPasswordComparer;
 import com.aierview.backend.auth.usecase.contract.ILocalSignin;
 import com.aierview.backend.shared.testdata.AuthTestFixture;
 import org.junit.jupiter.api.BeforeEach;
@@ -18,11 +22,15 @@ import static org.mockito.Mockito.*;
 public class LocalSigninTests {
     private ILocalSignin localSignin;
     private IUserRepository userRepository;
+    private IAuthRepository authRepository;
+    private IPasswordComparer passwordComparer;
 
     @BeforeEach
     void setUp() {
         this.userRepository = mock(IUserRepository.class);
-        this.localSignin = new LocalSignin(this.userRepository);
+        this.authRepository = mock(IAuthRepository.class);
+        this.passwordComparer = mock(IPasswordComparer.class);
+        this.localSignin = new LocalSignin(this.userRepository, authRepository, passwordComparer);
     }
 
 
@@ -38,5 +46,25 @@ public class LocalSigninTests {
         assertThat(exception).isInstanceOf(InvalidCredentialException.class);
         assertThat(exception.getMessage()).isEqualTo("Email or password is incorrect!");
         verify(this.userRepository, times(1)).findByEmail(request.getEmail());
+    }
+
+    @Test
+    @DisplayName("Should throw InvalidCredentialsException when password does not match")
+    void shouldThrowInvalidCredentialsExceptionWhenPasswordDoesNotMatch() {
+        LocalSigninRequest request = AuthTestFixture.anyLocalSigninRequest();
+        UserRef savedUser = AuthTestFixture.anySavedUserRef();
+        Auth savedAuth = AuthTestFixture.anySavedAuth(savedUser);
+
+        when(this.userRepository.findByEmail(request.getEmail())).thenReturn(Optional.of(savedUser));
+        when(this.authRepository.findByUserId(savedUser.getId())).thenReturn(Optional.of(savedAuth));
+        when(this.passwordComparer.matches(request.getPassword(), savedAuth.getPassword())).thenReturn(false);
+
+        Throwable exception = catchThrowable(() -> this.localSignin.execute(request));
+
+        assertThat(exception).isInstanceOf(InvalidCredentialException.class);
+        assertThat(exception.getMessage()).isEqualTo("Email or password is incorrect!");
+        verify(this.userRepository, times(1)).findByEmail(request.getEmail());
+        verify(this.authRepository, times(1)).findByUserId(savedUser.getId());
+        verify(this.passwordComparer, times(1)).matches(request.getPassword(), savedAuth.getPassword());
     }
 }
