a70b80ccc1e22bdb972e8abfd7702c7f51db2528
gervasioartur <gervasioartur@outlook.com.br>
2025-08-06 04:30:18 -0300

    feat: ensure AuthController returns 401 if user does not exist on local signin

--- src/main/java/com/aierview/backend/auth/infra/controller/AuthController.java
+++ src/main/java/com/aierview/backend/auth/infra/controller/AuthController.java
@@ -1,14 +1,20 @@
 package com.aierview.backend.auth.infra.controller;
 
+import com.aierview.backend.auth.domain.model.CookieResponse;
+import com.aierview.backend.auth.domain.model.LocalSigninRequest;
 import com.aierview.backend.auth.domain.model.LocalSignupRequest;
 import com.aierview.backend.auth.domain.model.Response;
+import com.aierview.backend.auth.usecase.contract.ILocalSignin;
 import com.aierview.backend.auth.usecase.contract.ILocalSignup;
+import com.aierview.backend.shared.utils.FuncUtils;
 import io.swagger.v3.oas.annotations.Operation;
 import io.swagger.v3.oas.annotations.responses.ApiResponse;
 import io.swagger.v3.oas.annotations.responses.ApiResponses;
 import io.swagger.v3.oas.annotations.tags.Tag;
+import jakarta.servlet.http.HttpServletResponse;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
+import org.springframework.http.HttpHeaders;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
@@ -22,6 +28,7 @@ import org.springframework.web.bind.annotation.RestController;
 @Tag(name = "Authentication", description = "Authentication Features")
 public class AuthController {
     private final ILocalSignup localSignupUseCase;
+    private final ILocalSignin localSigninUseCase;
 
     @PostMapping("/local/signup")
     @Operation(summary = "Local signup")
@@ -34,5 +41,20 @@ public class AuthController {
         this.localSignupUseCase.execute(request);
         Response response = Response.builder().data("Created").statusCode(HttpStatus.CREATED.value()).build();
         return new ResponseEntity<>(response, HttpStatus.CREATED);
+    }
+
+    @PostMapping("/local/signin")
+    @Operation(summary = "Local signin")
+    @ApiResponses(value = {
+            @ApiResponse(responseCode = "200", description = "OK"),
+            @ApiResponse(responseCode = "401", description = "UNAUTHORIZED"),
+            @ApiResponse(responseCode = "500", description = "INTERNAL_SERVER_ERROR")
+    })
+    public ResponseEntity<Response> localSignin(@Valid @RequestBody LocalSigninRequest request, HttpServletResponse httPResponse) {
+        CookieResponse cookieResponse = this.localSigninUseCase.execute(request);
+        String cookie = FuncUtils.cookieFromCookieResponse(cookieResponse);
+        Response response = Response.builder().statusCode(HttpStatus.OK.value()).build();
+        httPResponse.addHeader(HttpHeaders.SET_COOKIE, cookie);
+        return ResponseEntity.ok().body(response);
     }
 }
