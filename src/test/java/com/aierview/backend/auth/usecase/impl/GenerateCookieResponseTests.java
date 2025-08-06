b6b4442bb2ebac1a08fe2f0780b77e40e09974b1
gervasioartur <gervasioartur@outlook.com.br>
2025-08-06 01:35:19 -0300

    test: ensure GenerateCookieResponse returns cookie response for dev env

--- src/test/java/com/aierview/backend/auth/usecase/impl/GenerateCookieResponseTests.java
+++ src/test/java/com/aierview/backend/auth/usecase/impl/GenerateCookieResponseTests.java
@@ -37,9 +37,27 @@ public class GenerateCookieResponseTests {
     @DisplayName("Should return cookie response for homolog env")
     void shouldReturnCookieResponseForHomologEnv() {
         String token = UUID.randomUUID().toString();
-        CookieResponse cookieResponse = AuthTestFixture.anyProdCookieResponse(token);
+        CookieResponse cookieResponse = AuthTestFixture.anyHomologCookieResponse(token);
 
         this.generateCookieResponse = new GenerateCookieResponse(Environment.HOMOLOG);
+        CookieResponse response = this.generateCookieResponse.generate("token", token);
+
+        Assertions.assertNotNull(this.generateCookieResponse);
+        Assertions.assertEquals(response.name(),cookieResponse.name());
+        Assertions.assertEquals(response.value(),cookieResponse.value());
+        Assertions.assertEquals(response.httpOnly(),cookieResponse.httpOnly());
+        Assertions.assertEquals(response.secure(),cookieResponse.secure());
+        Assertions.assertEquals(response.sameSite(),cookieResponse.sameSite());
+        Assertions.assertEquals(response.path(),cookieResponse.path());
+    }
+
+    @Test
+    @DisplayName("Should return cookie response for dev env")
+    void shouldReturnCookieResponseForDevEnv() {
+        String token = UUID.randomUUID().toString();
+        CookieResponse cookieResponse = AuthTestFixture.anyDevCookieResponse(token);
+
+        this.generateCookieResponse = new GenerateCookieResponse(Environment.DEV);
         CookieResponse response = this.generateCookieResponse.generate("token", token);
 
         Assertions.assertNotNull(this.generateCookieResponse);
