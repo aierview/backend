ce2ed94cf570ff4221e8302c779c59c2c15e0eeb
gervasioartur <gervasioartur@outlook.com.br>
2025-08-08 01:28:17 -0300

    feat: ensure GoogleSignup saves user and account details on signup success

--- src/main/java/com/aierview/backend/auth/usecase/impl/GoogleSignup.java
+++ src/main/java/com/aierview/backend/auth/usecase/impl/GoogleSignup.java
@@ -1,9 +1,13 @@
 package com.aierview.backend.auth.usecase.impl;
 
+import com.aierview.backend.auth.domain.entity.Auth;
 import com.aierview.backend.auth.domain.entity.UserRef;
+import com.aierview.backend.auth.domain.enums.AuthProvider;
 import com.aierview.backend.auth.domain.exceptions.EmailAlreadyInUseException;
 import com.aierview.backend.auth.domain.exceptions.InvalidGoogleIdTokenException;
 import com.aierview.backend.auth.domain.google.IExtractUserDetails;
+import com.aierview.backend.auth.domain.model.GoogleAccountModel;
+import com.aierview.backend.auth.domain.repository.IAuthRepository;
 import com.aierview.backend.auth.domain.repository.IUserRepository;
 import com.aierview.backend.auth.usecase.contract.IGoogleSignup;
 
@@ -12,16 +16,22 @@ import java.util.Optional;
 public class GoogleSignup implements IGoogleSignup {
     private final IExtractUserDetails extractUserDetails;
     private final IUserRepository userRepository;
+    private final IAuthRepository authRepository;
 
-    public GoogleSignup(IExtractUserDetails extractUserDetails, IUserRepository userRepository) {
+    public GoogleSignup(IExtractUserDetails extractUserDetails, IUserRepository userRepository, IAuthRepository authRepository) {
         this.extractUserDetails = extractUserDetails;
         this.userRepository = userRepository;
+        this.authRepository = authRepository;
     }
 
     @Override
     public void execute(String idToken) {
-        var googleAccountModel = this.extractUserDetails.extract(idToken).orElseThrow(InvalidGoogleIdTokenException::new);
-        Optional<UserRef>  userRef =  this.userRepository.findByEmail(googleAccountModel.email());
-        if(userRef.isPresent()) throw new EmailAlreadyInUseException(googleAccountModel.email());
+        GoogleAccountModel googleAccountModel = this.extractUserDetails.extract(idToken).orElseThrow(InvalidGoogleIdTokenException::new);
+        Optional<UserRef>  existingUser =  this.userRepository.findByEmail(googleAccountModel.email());
+        if(existingUser.isPresent()) throw new EmailAlreadyInUseException(googleAccountModel.email());
+        UserRef user =  UserRef.builder().name(googleAccountModel.name()).email(googleAccountModel.email()).build();
+        user = this.userRepository.save(user);
+        Auth  auth =  Auth.builder().user(user).provider(AuthProvider.GOOGLE).picture(googleAccountModel.picture()).build();
+        this.authRepository.save(auth);
     }
 }
