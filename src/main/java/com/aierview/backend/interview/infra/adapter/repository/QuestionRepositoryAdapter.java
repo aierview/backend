b0beecaa4deeafb9d8e89e1febdc8a25fc35a1be
gervasioartur <gervasioartur@outlook.com.br>
2025-08-16 12:40:38 -0300

    feat: adding findbyid

--- src/main/java/com/aierview/backend/interview/infra/adapter/repository/QuestionRepositoryAdapter.java
+++ src/main/java/com/aierview/backend/interview/infra/adapter/repository/QuestionRepositoryAdapter.java
@@ -9,6 +9,7 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Repository;
 
 import java.util.List;
+import java.util.Optional;
 
 @Repository
 @RequiredArgsConstructor
@@ -16,11 +17,25 @@ public class QuestionRepositoryAdapter implements IQuestionRepository {
     private final QuestionMapper questionMapper;
     private final QuestionJpaRepository questionJpaRepository;
 
+    @Override
+    public Question save(Question question) {
+        QuestionJpaEntity entity = this.questionMapper.mapToJpa(question);
+        entity = this.questionJpaRepository.save(entity);
+        return this.questionMapper.mapToEntity(entity);
+    }
 
     @Override
     public List<Question> saveAll(List<Question> questions) {
-        List<QuestionJpaEntity> entities = this.questionMapper.mapToJpa(questions);
+        List<QuestionJpaEntity> entities = this.questionMapper.mapToListJpa(questions);
         entities = this.questionJpaRepository.saveAll(entities);
-        return this.questionMapper.mapToEntity(entities);
+        return this.questionMapper.mapToListEntity(entities);
+    }
+
+    @Override
+    public Optional<Question> findById(Long id) {
+        Optional<QuestionJpaEntity> jpaEntity = this.questionJpaRepository.findById(id);
+        if (jpaEntity.isEmpty()) return Optional.empty();
+        Question question = this.questionMapper.mapToEntity(jpaEntity.get());
+        return Optional.of(question);
     }
 }
