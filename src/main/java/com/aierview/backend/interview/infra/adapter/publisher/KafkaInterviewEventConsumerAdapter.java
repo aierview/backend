b4e6ff60d560e0ced14df64dec7d8a9ff1bbc8a9
gervasioartur <gervasioartur@outlook.com.br>
2025-08-16 12:39:20 -0300

    feat: adding KafkaInterviewEventConsumerAdapter

---
+++ src/main/java/com/aierview/backend/interview/infra/adapter/publisher/KafkaInterviewEventConsumerAdapter.java
@@ -0,0 +1,20 @@
+package com.aierview.backend.interview.infra.adapter.publisher;
+
+import com.aierview.backend.interview.domain.contract.publisher.IKafkaInterviewEventConsumer;
+import com.aierview.backend.interview.domain.model.CurrentQuestion;
+import com.aierview.backend.interview.usecase.contract.ISendCurrentQuestion;
+import lombok.RequiredArgsConstructor;
+import org.springframework.kafka.annotation.KafkaListener;
+import org.springframework.stereotype.Component;
+
+@Component
+@RequiredArgsConstructor
+public class KafkaInterviewEventConsumerAdapter implements IKafkaInterviewEventConsumer {
+    private final ISendCurrentQuestion sendCurrentQuestion;
+
+    @Override
+    @KafkaListener(topics = "interview.next-question-audio", groupId = "backend-service-group")
+    public void consume(CurrentQuestion message) {
+        this.sendCurrentQuestion.execute(message);
+    }
+}
