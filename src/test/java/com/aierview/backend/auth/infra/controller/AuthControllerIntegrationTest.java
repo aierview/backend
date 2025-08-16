96dfe6fa75b4cef0738a25c128052df25cab2b7c
gervasioartur <gervasioartur@outlook.com.br>
2025-08-15 21:39:27 -0300

    test: fixing AuthControllerIntegration tests

--- src/test/java/com/aierview/backend/auth/infra/controller/AuthControllerIntegrationTest.java
+++ src/test/java/com/aierview/backend/auth/infra/controller/AuthControllerIntegrationTest.java
@@ -30,9 +30,12 @@ import org.springframework.test.context.DynamicPropertyRegistry;
 import org.springframework.test.context.DynamicPropertySource;
 import org.springframework.test.web.servlet.MockMvc;
 import org.springframework.test.web.servlet.request.MockHttpServletRequestBuilder;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.KafkaContainer;
 import org.testcontainers.containers.PostgreSQLContainer;
 import org.testcontainers.junit.jupiter.Container;
 import org.testcontainers.junit.jupiter.Testcontainers;
+import org.testcontainers.utility.DockerImageName;
 
 import static com.github.tomakehurst.wiremock.client.WireMock.*;
 import static org.springframework.boot.test.context.SpringBootTest.WebEnvironment.RANDOM_PORT;
@@ -45,6 +48,12 @@ import static org.springframework.test.web.servlet.result.MockMvcResultMatchers.
 @ActiveProfiles("test")
 @SpringBootTest(properties = "spring.profiles.active=test", webEnvironment = RANDOM_PORT)
 public class AuthControllerIntegrationTest {
+    static GenericContainer<?> redisContainer = new GenericContainer<>(DockerImageName.parse("redis:7.2.0-alpine"))
+            .withExposedPorts(6379);
+
+    @Container
+    static KafkaContainer kafkaContainer = new KafkaContainer(DockerImageName.parse("confluentinc/cp-kafka:7.2.1"));
+
     @Container
     static PostgreSQLContainer<?> postgresContainer = new PostgreSQLContainer<>("postgres:15")
             .withDatabaseName("testdb")
@@ -69,10 +78,17 @@ public class AuthControllerIntegrationTest {
         registry.add("spring.datasource.url", postgresContainer::getJdbcUrl);
         registry.add("spring.datasource.username", postgresContainer::getUsername);
         registry.add("spring.datasource.password", postgresContainer::getPassword);
+//      KAFKA CONFIG
+        registry.add("spring.kafka.bootstrap-servers", kafkaContainer::getBootstrapServers);
+//      REDIS CONFIG
+        registry.add("spring.data.redis.host", redisContainer::getHost);
+        registry.add("spring.data.redis.port", () -> redisContainer.getMappedPort(6379));
     }
 
     @BeforeAll
     static void startWireMockContainer() {
+        redisContainer.start();
+
         wireMockServer = new WireMockServer(8089);
         wireMockServer.start();
 
